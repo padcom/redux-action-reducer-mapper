7e24a621666802bdff2a9a26017a1ed27a059225
Matthias Hryniszak <padcom@gmail.com>
2016-06-28 01:12:38 +0200

    Code cleanup

--- index.js
+++ index.js
@@ -3,12 +3,9 @@
  */
 module.exports = function mapActionToReducer(reducers) {;
   return function(state, action) {
-    console.log(typeof reducers[action.type]);
     if (typeof reducers[action.type] === 'function') {
-      console.log('FUNCTION!!');
       return reducers[action.type](state, action);
     } else {
-      console.log('not a function?');
       return state || reducers['default'];
     }
   }
